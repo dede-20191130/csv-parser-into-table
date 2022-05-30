4f1756d45b9a8c943d57476e73f04c9cc964c8b7
dede_20191130 <1044adad@gmail.com>
2022-05-30 17:06:10 +0900

    cons app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, Paper, styled, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
+import { ChangeEventHandler, useState } from 'react';
+import Papa from 'papaparse';
+
+const Input = styled('input')({
+  display: 'none',
+});
+
+function checkIfUrl(str: string) {
+  return (/^https?\:\/\//.test(str));
+}
 
 function App() {
+  const [fileData, setFileData] = useState<string[][]>([]);
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (event) => {
+    if (!(
+      event.target.files !== null &&
+      event.target?.files?.length > 0
+    )) return;
+
+    const tgtFile = event.target.files[0];
+    if (tgtFile.type !== "text/csv") return;
+
+    Papa.parse<string[], File>(tgtFile, {
+      header: false,
+      complete: (results) => {
+        setFileData(results.data);
+      }
+    })
+
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Typography sx={{ fontSize: "2rem" }} variant='h1'>CSV Parser Into Table</Typography>
+      <Box sx={{ textAlign: "center", mb: 3 }}>
+        <label htmlFor="contained-button-file">
+          <Input accept="text/csv" id="contained-button-file" type="file" onChange={handleChange} />
+          <Button variant="contained" component="span">
+            Import
+          </Button>
+        </label>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableBody>
+            {fileData.map((fileDataLine) => (
+              <TableRow
+                key={fileDataLine.join("-")}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                {fileDataLine.map((item) => {
+                  const cellContent = checkIfUrl(item.trim()) ? <a href={item} target="_blank">{item}</a> : item;
+                  return <TableCell key={item} align="center">{cellContent}</TableCell>
+                })}
+
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
 
